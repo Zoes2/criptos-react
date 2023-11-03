719d9f2fb233b858d030cb9b8f0aa41a3704f644
Zoes2 <v.meneses20.20@gmail.com>
2023-11-03 17:58:41 -0300

    conectamos la API de criptos y lo almacenamos

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
 import styled from '@emotion/styled'
 import useSelectMonedas from '../hooks/useSelectMonedas'
 import { monedas } from '../data/monedas'
@@ -24,14 +24,38 @@ const InputSubmit = styled.input`
 
 const Formulario = () => {
 
+    const [criptos, setCriptos] = useState([])
 
-    const [ SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas)
+
+    useEffect(() => {
+        const consultarAPI = async () => {
+            const url = 'https://min-api.cryptocompare.com/data/top/mktcapfull?limit=10&tsym=USD'
+            const respuesta = await fetch(url)
+            const resultado = await respuesta.json()
+
+            const arrayCriptos = resultado.Data.map( cripto => {
+
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+                return objeto
+            })
+
+            setCriptos(arrayCriptos)
+        }
+        consultarAPI()
+    }, [])
+    
 
 
     return (
         <form>
 
             <SelectMonedas />
+
+            
 
             <InputSubmit
                 type="submit"
